ec46ed6e23ae45cc4acb037f410ad03a6f671f6f
Tyler Heucke <tgheucke@gmail.com>
2014-12-28 20:29:39 -0800

    Switch from UIAlertView to UIAlertController

--- TaskTracker/AddTaskTableViewController.swift
+++ TaskTracker/AddTaskTableViewController.swift
@@ -51,8 +51,22 @@ class AddTaskTableViewController: UITableViewController, UITextFieldDelegate, UI
   
   @IBAction func cancelButtonPressed(sender: UIBarButtonItem) {
     if self.titleTextField.text.utf16Count > 0 && self.titleTextField.text != self.taskToEdit?.title {
-      let alert = UIAlertView(title: "Warning", message: "You have entered a title. Are you sure you would like to cancel?", delegate: self, cancelButtonTitle: "No", otherButtonTitles: "Yes")
-      alert.show()
+      
+      let alertController = UIAlertController(title: "Warning", message: "You have entered a title. Are you sure you would like to cancel?", preferredStyle: .Alert)
+      
+      let cancelAction = UIAlertAction(title: "No", style: .Cancel) { (action) in
+        alertController.dismissViewControllerAnimated(true, {})
+      }
+      alertController.addAction(cancelAction)
+      
+      let okAction = UIAlertAction(title: "Yes", style: .Default) { (action) in
+        self.navigationController?.popViewControllerAnimated(true)
+        alertController.dismissViewControllerAnimated(true, {})
+      }
+      alertController.addAction(okAction)
+      
+      self.presentViewController(alertController, animated: true, completion: {})
+      
     } else {
       self.navigationController?.popViewControllerAnimated(true)
     }
@@ -60,25 +74,39 @@ class AddTaskTableViewController: UITableViewController, UITextFieldDelegate, UI
   
   func doneAction() {
     if self.titleTextField.text.utf16Count == 0 {
-      let alert = UIAlertView(title: "Error", message: "You must input a title for the task.", delegate: self, cancelButtonTitle: "OK")
-      alert.show()
+
+      let alertController = UIAlertController(title: "Error", message: "You must input a title for this task", preferredStyle: .Alert)
+      
+      let cancelAction = UIAlertAction(title: "OK", style: .Cancel) { (action) in
+        alertController.dismissViewControllerAnimated(true, {})
+      }
+      alertController.addAction(cancelAction)
+      
+      self.presentViewController(alertController, animated: true, completion: {})
+      
     } else {
       
       let realm = RLMRealm.defaultRealm()
+      
       if let task = taskToEdit {
+        
         realm.transactionWithBlock() {
           task.title = self.titleTextField.text
           task.topic = self.topicTextField.text
           task.dueDate = DateHelpers.dateWithNoTime(date: self.datePicker.date)
         }
+        
       } else {
+        
         var task = Task()
         task.title = self.titleTextField.text
         task.topic = self.topicTextField.text
         task.dueDate = DateHelpers.dateWithNoTime(date: self.datePicker.date)
+        
         realm.transactionWithBlock() {
           realm.addObject(task)
         }
+        
       }
       
       self.navigationController?.popViewControllerAnimated(true)
@@ -88,20 +116,16 @@ class AddTaskTableViewController: UITableViewController, UITextFieldDelegate, UI
   
   // MARK: - Helpers
   
-  // Dismiss view controller if  user chooses to
-  func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
-    if buttonIndex == 1 {
-      self.navigationController?.popViewControllerAnimated(true)
-    }
-  }
-  
   func textFieldShouldReturn(textField: UITextField) -> Bool {
+    
     if textField == self.titleTextField { // Move focus to topic text field
       self.topicTextField.becomeFirstResponder()
     } else if textField == self.topicTextField { // Finish adding task
       self.doneAction()
     }
+    
     return true
+    
   }
   
 }
